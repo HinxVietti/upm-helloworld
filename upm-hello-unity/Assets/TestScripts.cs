f4987ef6582b23190461d7bab76fa2ead5e3d83c
Hinx Vietti <hinxvietti@gmail.com>
2021-03-10 16:05:30 +0800

    init commit

---
+++ upm-hello-unity/Assets/TestScripts.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class TestScripts
+{
+
+
+    static TestScripts()
+    {
+        Debug.Log("Unity Loaded . this is " + Application.productName);
+    }
+
+
+}
